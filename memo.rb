5d5a61be3012379be94d914fc6a9d81eef174822
kobayoshi-xs <82309189+kobayoshi-xs@users.noreply.github.com>
2021-06-08 13:27:04 +0900

    ファイル名?になる？修正とEnterで処理が終わってしまう修正

--- memo.rb
+++ memo.rb
@@ -6,13 +6,13 @@ memo_type = gets
 if memo_type.to_i == 1
   p "拡張子を除いたファイルを入力して下さい"
   memo_type2 = gets
+   p "メモしたい内容を記入して下さい"
+   p "終了したらCtrl + Dを押して下さい"
+  memo_type3 = $stdin.read
   CSV.open("#{memo_type2}.csv", "w") do |csv|
-  end
-  p "メモしたい内容を記入して下さい"
-  memo_type3 = gets.chomp
-  CSV.open("#{memo_type2}.csv", "a") do |csv|
     csv << ["#{memo_type3}"]
   end
+end
 elsif memo_type.to_i == 2
   p "編集を行うファイル名を入力して下さい"
   memo_type4 = gets
@@ -20,9 +20,10 @@ elsif memo_type.to_i == 2
     p row
   end
   p "追加したいデータを入力して下さい"
-  memo_type5 = gets.chomp
+  p "終了したらCtrl + Dを押して下さい"
+  memo_type5 = $stdin.read
   CSV.open("#{memo_type4}.csv", "a") do |csv|
-  csv << ["#{memo_type5}"]
+    csv << ["#{memo_type5}"]
   end
 else
   p "数字の1か2を入力して下さい"
